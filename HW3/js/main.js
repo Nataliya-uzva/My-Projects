8ba2a4171fcecd229d4ce6690fee1da0d5d8f1a0
Nataliya Uzva <ashatan_u@mail.ru>
2015-12-08 10:41:18 +0200

    array3x3

---
+++ HW3/js/main.js
@@ -0,0 +1,9 @@
+var arr1 = ['a', 'b', 'c'];
+var arr2 = ['D', 'E', 'F'];
+var arr3 = [1, 2, 3];
+var arr = [arr1, arr2, arr3];
+for (var i = 0; i< arr.length; i++) {
+	for (var j = 0; j < arr.length; j++) {
+		console.log (arr[i][j]);
+	}
+}
